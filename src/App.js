8c25c873ef4abc195eb71ade7c57176b64489b27
Ahtesham <ahtesham2000@gmail.com>
2021-09-17 12:23:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useEffect } from 'react'
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import 'bootstrap-scss';
+import '../src/assets/scss/flaticon.scss';
+import '../src/assets/scss/font-awesome.scss';
+import "../src/assets/scss/color-1.scss"
+import '../src/assets/scss/themify.scss';
+import "../src/assets/scss/slick.scss";
+import "../src/assets/scss/slick-theme.scss";
+// import "./App.scss";
+import Header from "./components/common/Header";
+import LayoutPage from "./screens/LayoutPage";
+
+
+const App = () => {
+  useEffect(() => {
+    document.body.classList.add('try');
+    document.body.style.setProperty('--primary', '#fb3b64')
+    document.body.style.setProperty('--secondary', '#071828')
+    document.body.style.setProperty('--light', '#071828')
+    document.body.style.setProperty('--dark', '#fb3b64')
+  })
+  return (
+    <Fragment>
+      <div>
+        <div>
+          <BrowserRouter>
+            <Header className="sass1" />
+            <Switch>
+              <Route exact path="/" component={LayoutPage} />
+            </Switch>
+
+          </BrowserRouter>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/screens/LayoutPage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+// import Custom Components
+// import Header from '../components/common/Header'
+import BannerSection from "../components/modern-sass/banner";
+import FeatureSection from "../components/modern-sass/feature";
+import BuildSection from "../components/modern-sass/build";
+import WorkSection from "../components/modern-sass/work";
+import PriceSection from "../components/modern-sass/price";
+import TestimonialSection from "../components/modern-sass/testimonial";
+import SubscribeSection from "../components/modern-sass/subscribe";
+import FaqSection from "../components/modern-sass/faq";
+import ClientSection from "../components/modern-sass/client";
+import FooterSection from "../components/modern-sass/footer";
+import Copyright from '../components/modern-sass/copyright';
+
+const LayoutPage = () => {
+  return (
+    <>
+      {/* <Header className="saas1" /> */}
+  
+      <BannerSection />
+
+      <FeatureSection />
+
+      <BuildSection />
+
+      <WorkSection />
+
+      <PriceSection />
+
+      <TestimonialSection />
+
+      <SubscribeSection />
+
+      <FaqSection />
+
+      <ClientSection />
+
+      <FooterSection />
+
+      <Copyright />
+    </>
+  );
+};
+
+export default LayoutPage;
